73eeb11d9d779e5bd4dc9c942d2a4c523ba8893d
Mikhail Malakhov <malakhv@gmail.com>
2025-08-09 19:32:26 +0300

    Updated test.py

--- test/test.py
+++ test/test.py
@@ -29,6 +29,78 @@ import pykit
 from pykit import process
 from pykit import git
 
+import subprocess as _sp
+from subprocess import CalledProcessError as _cpe
+
+from os.path import abspath as _ap
+from os import getcwd as _cwd
+
+#-------------------------------------------------------------------------------
+# Present Working Directory
+#-------------------------------------------------------------------------------
+
+""" Returns present working directory. """
+def pwd():
+    return _ap(_cwd())
+# END
+
+#-------------------------------------------------------------------------------
+# Execute external process/command
+#-------------------------------------------------------------------------------
+
+""" Executes specified process/command with parameters. """
+def exec(proc, *args, shell = False, silent = False,  out_prefix = '', \
+        out_postfix = '', out_keep_empty_lines = True):
+
+    result = ''
+
+    # Prepare command
+    cmd = [proc]
+    if args : cmd.extend(args)
+
+    # Want to use default output?
+    custom_out = silent or (not out_keep_empty_lines)
+
+    # Output prefix?
+    if out_prefix:
+        if (not custom_out):
+            print(out_prefix)
+        else:
+            result = out_prefix + '\n'
+
+    # Call
+    process = None
+    try:
+        process = _sp.run(cmd, shell = shell, check = True, \
+            capture_output = custom_out, text = custom_out)
+    except _cpe as e:
+        if (not custom_out):
+            print(e.stderr)
+        else:
+            result = e.stderr
+        return
+
+    if (custom_out):
+        result += process.stdout
+
+    if (not out_keep_empty_lines):
+        result = result.replace('\n\n','\n')
+
+    # Output postfix?
+    if (out_postfix):
+        if (not custom_out):
+            print(out_postfix)
+        else:
+            result += '\n' + out_postfix
+
+    # Need a result?
+    if (custom_out):
+        return result
+    else:
+        return None
+
+# END
+
 #-------------------------------------------------------------------------------
 # Script entry point
 #-------------------------------------------------------------------------------
@@ -41,25 +113,28 @@ if __name__ == '__main__':
     
     # PWD
     print(pykit.pwd())
+    print(pwd())
 
     # Process
     print()
-    out = process.exec('git', 'status', out_keep_empty_lines=True, silent=True)
+    out = process.exec('git', 'status', out_keep_empty_lines=True, silent=False)
+    #out = _sp.call(['git', 'status'])
     if (out) : print(out)
+
 
     # Git
     #print('-----------------------')
-    print('REPO: PyKit')
-    print('-----------------------')
-    out = git.git_status()
-    if (out) : print(out)
+    #print('REPO: PyKit')
+    #print('-----------------------')
+    #out = git.git_status()
+    #if (out) : print(out)
 
     # Git
-    print()
-    print()
+    #print()
+    #print()
     #print('-----------------------')
-    print('REPO: PascalKit')
-    print('-----------------------')
-    out = git.git_status()
-    if (out) : print(out)
+    #print('REPO: PascalKit')
+    #print('-----------------------')
+    #out = git.git_status()
+    #if (out) : print(out)
 #-------------------------------------------------------------------------------
